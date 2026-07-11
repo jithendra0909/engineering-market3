28750780a1b4dc8ec23134d59d9b0e77747bcc2d
jithendra0909 <jithendra.xx@gmail.com>
2026-07-11 19:38:44 +0530

    Auto-seed colleges in getColleges controller if missing

--- server/src/controllers/collegeController.js
+++ server/src/controllers/collegeController.js
@@ -5,6 +5,19 @@ import College from '../models/College.js';
 // @access  Public
 const getColleges = async (req, res) => {
   try {
+    // Ensure both college records exist in database
+    const collegesList = [
+      "Vignan's Institute of Information Technology (VIIT)",
+      "Vignan's Institute of Engineering for Women (VIEW)"
+    ];
+
+    for (const name of collegesList) {
+      const exists = await College.findOne({ name });
+      if (!exists) {
+        await College.create({ name, isActive: true });
+      }
+    }
+
     const colleges = await College.find({ isActive: true }).select('name');
     res.json(colleges);
   } catch (error) {
